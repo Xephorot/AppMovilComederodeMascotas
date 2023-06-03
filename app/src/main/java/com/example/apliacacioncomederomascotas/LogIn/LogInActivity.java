e139263c9e3c2d7198d91d6b477bab79f607c3f7
Xephorot <escalantesergio821@gmail.com>
2023-06-03 10:51:51 -0400

    LogIn Creado

---
+++ app/src/main/java/com/example/apliacacioncomederomascotas/LogIn/LogInActivity.java
@@ -0,0 +1,16 @@
+package com.example.apliacacioncomederomascotas.LogIn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.apliacacioncomederomascotas.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.apliacacioncomederomascotas.R.layout.activity_log_in);
+    }
+}
