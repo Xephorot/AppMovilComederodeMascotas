04113e7b8431a94a93115fdbca23c67cefdfe6a5
Xephorot <escalantesergio821@gmail.com>
2023-06-03 11:13:06 -0400

    LogIn Creado

---
+++ app/src/main/java/com/example/apliacacioncomederomascotas/BotonDispensar/BotonDispensarActivity.java
@@ -0,0 +1,16 @@
+package com.example.apliacacioncomederomascotas.BotonDispensar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.apliacacioncomederomascotas.R;
+
+public class BotonDispensarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boton_dispensar);
+    }
+}
